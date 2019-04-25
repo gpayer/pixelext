db8c29e6d8c2d47d33b12c01b3639e9d01dca1a5
Gernot Messow <gernot@gernot-payer.de>
2019-04-25 19:11:27 +0200

    InputBox: implemented cursor movement, deleting and arbitrary input (still WIP)

--- ui/inputbox.go
+++ ui/inputbox.go
@@ -12,14 +12,17 @@ type InputBox struct {
 	UIBase
 	background    *nodes.BorderBox
 	content       string
+	sub           *nodes.SubScene
 	text          *Text
 	cursor        *nodes.Canvas
 	cursorpos     int
+	textscroll    float64
 	blinkduration float64
 	totaltime     float64
 	disabled      bool
 	focused       bool
 	cursorshown   bool
+	maxlen        int
 	onchange      func(string)
 	onenter       func(string)
 }
@@ -28,14 +31,17 @@ func NewInputBox(name, atlasname string, w, h float64) *InputBox {
 	i := &InputBox{
 		UIBase:        *NewUIBase(name),
 		background:    nodes.NewBorderBox("bbox", w, h),
+		sub:           nodes.NewSubScene("sub", w-8, h-6),
 		text:          NewText("txt", atlasname),
-		cursor:        nodes.NewCanvas("cursor", 1, h-6),
+		cursor:        nodes.NewCanvas("cursor", 2, h-6),
 		content:       "",
+		textscroll:    0,
 		blinkduration: 0.5,
 		cursorpos:     0,
 		totaltime:     0,
 		focused:       false,
 		disabled:      false,
+		maxlen:        255,
 		onchange:      func(_ string) {},
 		onenter:       func(_ string) {},
 	}
@@ -47,39 +53,91 @@ func NewInputBox(name, atlasname string, w, h float64) *InputBox {
 }
 
 func (i *InputBox) Init() {
-	padding := i.GetStyles().Padding
-	size := i.Size()
+	//padding := i.GetStyles().Padding
+	//size := i.Size()
 	i.background.SetZIndex(-1)
 	i.AddChild(i.background)
+	i.sub.SetPos(pixel.V(0, 0))
+	i.AddChild(i.sub)
+	subroot := nodes.NewBaseNode("subroot")
+	i.sub.SetRoot(subroot)
 	i.text.SetAlignment(nodes.AlignmentCenterLeft)
-	i.text.SetPos(pixel.V(-size.X/2+padding, 0))
-	i.AddChild(i.text)
+	subroot.AddChild(i.text)
 	i.cursor.SetZIndex(1)
-	i.calcCursorPosition()
-	i.AddChild(i.cursor)
+	subroot.AddChild(i.cursor)
 	i.cursor.Hide()
+	i.recalc()
 }
 
-func (i *InputBox) calcCursorPosition() {
-	// TODO: take cursor position into account
-	i.cursor.SetPos(pixel.V(-i.Size().X/2+i.GetStyles().Padding+i.text.Size().X+2, 0))
+func (i *InputBox) recalc() {
+	if i.cursorpos > len(i.content) {
+		i.cursorpos = len(i.content)
+	} else if i.cursorpos < 0 {
+		i.cursorpos = 0
+	}
+	//padding := i.GetStyles().Padding
+	size := i.Size()
+	subwhalf := (size.X - 21) / 2
+	i.text.Clear()
+	i.text.Printf(i.content)
+	leftcursorcontent := i.content[0:i.cursorpos]
+	cursoroffset := i.text.Text().BoundsOf(leftcursorcontent).W() + 2
+	cursorposx := -subwhalf - i.textscroll + cursoroffset
+	if cursorposx < -subwhalf || cursorposx > subwhalf {
+		i.textscroll = -subwhalf + cursoroffset
+		if i.textscroll < 0 {
+			i.textscroll = 0
+		}
+		cursorposx = -subwhalf - i.textscroll + cursoroffset
+	}
+	//fmt.Printf("cursorposx: %f, textscroll: %f\n", cursorposx, i.textscroll)
+	i.text.SetPos(pixel.V(-subwhalf-i.textscroll, 0))
+	i.cursor.SetPos(pixel.V(cursorposx, 0))
 }
 
 func (i *InputBox) Update(dt float64) {
+	ev := nodes.Events()
 	if i.focused {
-		if nodes.Events().JustPressed(pixelgl.MouseButtonLeft) && !nodes.Events().Clicked(pixelgl.MouseButtonLeft, i) {
+		if ev.JustPressed(pixelgl.MouseButtonLeft) && !ev.Clicked(pixelgl.MouseButtonLeft, i) {
 			i.focused = false
 			i.cursor.Hide()
 			i.cursorshown = false
 			return
 		}
-		// TODO: backspace, cursor keys
-		typed := nodes.Events().Typed()
-		if len(typed) > 0 {
-			i.content += typed // TODO: take cursor position into account
-			i.text.Clear()
-			i.text.Printf(i.content)
-			i.calcCursorPosition()
+		if ev.JustPressed(pixelgl.KeyBackspace) || ev.Repeated(pixelgl.KeyBackspace) {
+			if i.cursorpos > 0 {
+				if i.cursorpos == 1 {
+					i.content = i.content[1:]
+				} else if i.cursorpos < len(i.content) {
+					i.content = i.content[:i.cursorpos-1] + i.content[i.cursorpos:]
+				} else {
+					i.content = i.content[:i.cursorpos-1]
+				}
+				i.cursorpos--
+				i.recalc()
+			}
+		} else if ev.JustPressed(pixelgl.KeyLeft) || ev.Repeated(pixelgl.KeyLeft) {
+			i.cursorpos--
+			i.recalc()
+		} else if ev.JustPressed(pixelgl.KeyRight) || ev.Repeated(pixelgl.KeyRight) {
+			i.cursorpos++
+			i.recalc()
+		} else { // TODO: more cursor keys
+			typed := ev.Typed()
+			if len(typed) > 0 && len(i.content) < i.maxlen {
+				if i.cursorpos == 0 {
+					i.content = typed + i.content
+				} else if i.cursorpos < len(i.content) {
+					i.content = i.content[:i.cursorpos] + typed + i.content[i.cursorpos:]
+				} else {
+					i.content += typed
+				}
+				i.cursorpos += len(typed)
+				if len(i.content) > i.maxlen {
+					i.content = i.content[:i.maxlen]
+				}
+				i.recalc()
+			}
 		}
 		i.totaltime += dt
 		if i.totaltime > 0.5 {
@@ -92,11 +150,13 @@ func (i *InputBox) Update(dt float64) {
 			i.cursorshown = !i.cursorshown
 		}
 	} else {
-		if nodes.Events().Clicked(pixelgl.MouseButtonLeft, i) {
-			i.focused = true
-			i.totaltime = 0
-			i.cursor.Show()
-			i.cursorshown = true
+		if ev.Clicked(pixelgl.MouseButtonLeft, i) {
+			if !i.focused {
+				i.focused = true
+				i.totaltime = 0
+				i.cursor.Show()
+				i.cursorshown = true
+			}
 			// TODO: calculate click location and set cursor accordingly
 		}
 	}
